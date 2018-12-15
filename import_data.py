82ae2de0cbb85cf3c49d4cb90e6211c1157201dc
Elijah Doern <elijah.doern@gmail.com>
2018-12-15 03:32:23 -0500

    Changed project focus
    Created data importer, cleaned up files, redesigned kmeans3 to classify properly

---
+++ import_data.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+csv = "mushroom.csv"
+ignore = "?"
+
+#### Dataset ####
+dataset_in = np.loadtxt(csv, delimiter=",", dtype=str)
+
+'''def reshape_input(input_arr):
+    output = np.array([[input_arr[0]]])
+    for i in range(1, input_arr.shape[0]):
+        output = np.vstack((output, np.array([[input_arr[i]]])))
+    return output'''
+
+def make_numeric(input_arr):
+	# transpose dataset
+	arr = input_arr.T
+	newTable = np.zeros(shape=(arr[0].shape[0]))
+	for i in range(arr.shape[0]):
+		if i == 10:
+			continue	# This skips feature 11: stalk-root, which has missing data
+		feature_map = {}
+		count = 0
+		feature = arr[i]
+		newRow = np.zeros(shape=(feature.shape[0]))
+		for x in range(feature.shape[0]):
+			if feature[x] not in feature_map:	# Add entry to the map of features if it isn't there already
+				feature_map[feature[x]] = count
+				count += 1
+			newRow[x] = feature_map[feature[x]]
+		newTable = np.vstack((newTable, newRow))
+	return newTable[1:].T
+
+X = make_numeric(dataset_in[1:])
--- kmeans3.py
+++ kmeans3.py
@@ -1,10 +1,12 @@
 import random
 import numpy as np
+import scipy.linalg as la
 import matplotlib.pyplot as plt
 from sklearn.cluster import MiniBatchKMeans as KMeans
+import import_data
 
 #### Dataset ####
-dataset_in = np.loadtxt("vehicle.csv", delimiter=",", dtype=str)
+#dataset_in = np.loadtxt("phonemes.csv", delimiter=",", dtype=str)
 
 def reshape_input(input_data):
     output = np.array([[input_data[0]]])
@@ -12,45 +14,42 @@ def reshape_input(input_data):
         output = np.vstack((output, np.array([[input_data[i]]])))
     return output
 
-X = reshape_input(dataset_in)
-# To get a vector of labels: X.T[18]
+#def regularize(input_data):
+
+
+X = import_data.X #reshape_input(dataset_in)
+# To get a vector of features: X.T[n,1:]
 # To get a single case vector: X[n].T
 # To get a 2D array of the data without labels: X[1:,0,:-1]
 
 
 
 #### Kmeans ####
-def run(input_arr, ):
-    kmeans = KMeans(n_clusters = 4, batch_size = 10) # initialization is performed using kmeans++
+def run(input_arr, nc, bs):
+    kmeans = KMeans(n_clusters = nc, batch_size = bs) # initialization is performed using kmeans++
     predictions = kmeans.fit_predict(input_arr) # Returns a list of values in {0...3}
-    labels = X.T[18][0]
-    clusters = [indices(0), indices(1), indices(2), indices(3)]
+    labels = X.T[21]
     count = 1
     def indices(i):
         return np.where(predictions == i)
+    clusters = [indices(0), indices(1)]
 
     for cluster in clusters:
-        van = 0;    bus = 0
-        saab = 0;   opel = 0
+        poisonous = 0; edible = 0
         for i in cluster[0]:
-            if labels[i] == 'van':
-                van += 1
-            elif labels[i] == 'bus':
-                bus += 1
-            elif labels[i] == 'saab':
-                saab += 1
-            elif labels[i] == 'opel':
-                opel += 1
+                poisonous += 1
+            elif labels[i] == 1:
+                edible += 1
         print("Cluster {}".format(count))
-        print("van:", van)
-        print("bus:", bus)
-        print("saab:", saab)
-        print("opel: {}\n".format(opel))
+        print("poisonous: {}".format(poisonous))
+        print("edible: {}\n".format(edible))
         count += 1
-
+    return clusters
 
 
 #### Testing ####
-original_data = X[1:,0,:-1]
+#original_data = X[1:,0,:-1]
+#single_vector = X[1:,0,5:8]
+#U,s,V = la.svd(original_data)
 
-run(original_data)
+clusters = run(X, 2, 250)
